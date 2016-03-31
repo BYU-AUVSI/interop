853c71603a2bff0de1c4e1d0a154ff375cc52b65
Jeff Burgin <jeffburgin@gmail.com>
2016-03-30 22:34:00 -0600

    trying to figure out what is happening to the cookie!

--- client.py
+++ client.py
@@ -102,15 +102,18 @@ def targetcallback(data):
 #
 def talker():
     print('Talking')
-    rospy.init_node('talker', anonymous=True)
-    publisher = rospy.Publisher('obstacles', String, 10)
+    # rospy.init_node('talker', anonymous=True)
+    publisher = rospy.Publisher('obstacles', String, queue_size=10)
     rate = rospy.Rate(10)
 
-    while not rospy.is_shutdown():
-        string = get_obstacles()
-        rospy.loginfo(string)
-        publisher.publish(string)
-        rate.sleep()
+    publisher.publish("one time")
+    print('Cookie: ' + get_cookie())
+
+    # while not rospy.is_shutdown():
+    #     string = get_obstacles()
+    #     rospy.loginfo(string)
+    #     publisher.publish(string)
+    #     rate.sleep()
 
 
 def main():
